8a909844234670ae663670ed4a3e1b65740effa5
Vicos <victor_joel_elbarto@hotmail.com>
2022-05-07 15:47:57 -0400

    Pagina educativa temrinada

---
+++ Pagina Educativa/Js/boton-copy.js
@@ -0,0 +1,187 @@
+const $content = document.getElementById('texto-ejemplo'),
+        $btn = document.getElementById('btn');
+
+$btn.addEventListener('click', e =>{
+    $content.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content1 = document.getElementById('texto-ejemplo-1'),
+        $btn1 = document.getElementById('btn-1');
+
+$btn1.addEventListener('click', e =>{
+    $content1.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content2 = document.getElementById('texto-ejemplo-2'),
+        $btn2 = document.getElementById('btn-2');
+
+$btn2.addEventListener('click', e =>{
+    $content2.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content3 = document.getElementById('texto-ejemplo-3'),
+        $btn3 = document.getElementById('btn-3');
+
+$btn3.addEventListener('click', e =>{
+    $content3.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content4 = document.getElementById('texto-ejemplo-4'),
+        $btn4 = document.getElementById('btn-4');
+
+$btn4.addEventListener('click', e =>{
+    $content4.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content5 = document.getElementById('texto-ejemplo-5'),
+        $btn5 = document.getElementById('btn-5');
+
+$btn5.addEventListener('click', e =>{
+    $content5.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content6 = document.getElementById('texto-ejemplo-6'),
+        $btn6 = document.getElementById('btn-6');
+
+$btn6.addEventListener('click', e =>{
+    $content6.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+const $content7 = document.getElementById('texto-ejemplo-7'),
+        $btn7 = document.getElementById('btn-7');
+
+$btn7.addEventListener('click', e =>{
+    $content7.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content8 = document.getElementById('texto-ejemplo-8'),
+        $btn8 = document.getElementById('btn-8');
+
+$btn8.addEventListener('click', e =>{
+    $content8.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content9 = document.getElementById('texto-ejemplo-9'),
+        $btn9 = document.getElementById('btn-9');
+
+$btn9.addEventListener('click', e =>{
+    $content9.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content10 = document.getElementById('texto-ejemplo-10'),
+        $btn10 = document.getElementById('btn-10');
+
+$btn10.addEventListener('click', e =>{
+    $content10.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content11 = document.getElementById('texto-ejemplo-11'),
+        $btn11 = document.getElementById('btn-11');
+
+$btn11.addEventListener('click', e =>{
+    $content11.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content12 = document.getElementById('texto-ejemplo-12'),
+        $btn12 = document.getElementById('btn-12');
+
+$btn12.addEventListener('click', e =>{
+    $content12.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content13 = document.getElementById('texto-ejemplo-13'),
+        $btn13 = document.getElementById('btn-13');
+
+$btn13.addEventListener('click', e =>{
+    $content13.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content14 = document.getElementById('texto-ejemplo-14'),
+        $btn14 = document.getElementById('btn-14');
+
+$btn14.addEventListener('click', e =>{
+    $content14.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content15 = document.getElementById('texto-ejemplo-15'),
+        $btn15 = document.getElementById('btn-15');
+
+$btn15.addEventListener('click', e =>{
+    $content15.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content16 = document.getElementById('texto-ejemplo-16'),
+        $btn16 = document.getElementById('btn-16');
+
+$btn16.addEventListener('click', e =>{
+    $content16.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content17 = document.getElementById('texto-ejemplo-17'),
+        $btn17 = document.getElementById('btn-17');
+
+$btn17.addEventListener('click', e =>{
+    $content17.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content18 = document.getElementById('texto-ejemplo-18'),
+        $btn18 = document.getElementById('btn-18');
+
+$btn18.addEventListener('click', e =>{
+    $content18.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content19 = document.getElementById('texto-ejemplo-19'),
+        $btn19 = document.getElementById('btn-19');
+
+$btn19.addEventListener('click', e =>{
+    $content19.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
+
+const $content20 = document.getElementById('texto-ejemplo-20'),
+        $btn20 = document.getElementById('btn-20');
+
+$btn20.addEventListener('click', e =>{
+    $content20.select();
+    alert("Texto copiado");
+    document.execCommand('copy');
+});
